b1b0e7e83b98944c28b62cef9c6c7c0e257c7f7a
nadjibamar <hello@ebabel.eu>
2015-10-22 02:32:16 +0200

    making progress with facebook login

--- src/js/scripts/authorisation/facebook.js
+++ src/js/scripts/authorisation/facebook.js
@@ -3,17 +3,43 @@
     'use strict';
 
     var ref = new Firebase('https://enchantment.firebaseio.com');
-
     var button = document.getElementById('facebook-login-button');
 
-    button.addEventListener('click', function (event) {
-        console.log(event);
+    button.addEventListener('click', function facebookLoginButton_onClick (event) { // jshint ignore:line
+
+        button.disabled = 'disabled';
+
+        // todo: show loading spinner.
 
         ref.authWithOAuthPopup('facebook', function (error, authData) {
+
+            // todo: hide loading spinner.
+            button.disabled = '';
+
             if (error) {
-                console.log('Login Failed!', error);
+                ref.child('error/authorisation/facebook').push(error);
+
+                // todo: display error message and give advice.
             } else {
-                console.log('Authenticated successfully with payload:', authData);
+                ebg.player = {
+                    id: authData.facebook.id,
+                    loginDate: new Date().toJSON(),
+                    displayName: authData.facebook.displayName,
+                    profileImageUrl: authData.facebook.profileImageURL,
+                    accessToken: authData.facebook.accessToken,
+                    firstName: authData.facebook.cachedUserProfile.first_name,
+                    lastName: authData.facebook.cachedUserProfile.last_name,
+                    timezone: authData.facebook.cachedUserProfile.timezone,
+                    locale: authData.facebook.cachedUserProfile.locale,
+                    gender: authData.facebook.cachedUserProfile.gender,
+                    ageRange: authData.facebook.cachedUserProfile.age_range.min
+                };
+
+                ref.child('user/facebook/' + ebg.player.id).push(ebg.player);
+
+                document.getElementById('login').style.display = 'none';
+
+                // todo: display name and profile image of logged in player.
             }
         });
     });
--- src/js/scripts/game/_namespace.js
+++ src/js/scripts/game/_namespace.js
@@ -1,9 +1,13 @@
 var ebg = {};
 
-ebg.version = '0.3.0';
+ebg.version = '0.3.1';
 
+// Custom error messages from the game are listed here and used throughout.
 ebg.err = {
     input: {
         required: 'Missing required input.'
     }
 };
+
+// Current player. This is undefined until a player logs in.
+ebg.player = undefined;
