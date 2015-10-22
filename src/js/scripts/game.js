c302fdc7a41d936574134eea98e13ee58e5d08aa
nadjibamar <hello@ebabel.eu>
2015-10-22 22:30:46 +0200

    improved animations

--- src/js/scripts/game.js
+++ src/js/scripts/game.js
@@ -24,11 +24,12 @@
     // Static spaceship.
     ebg.loadModel({
         path: 'models/spaceship/spaceship.dae',
-        name: 'static-spaceship',
+        name: 'hyper-spaceship',
         scene: scene,
         position: {
             x: -5,
-            z: 2
+            y: 20,
+            z: -50
         },
         rotation: {
             x: 30
@@ -72,9 +73,38 @@
         camera: camera,
         sprites: [
             {
-                // This spaceship is no animated because the orbit controls are used to look at it.
-                name: 'static-spaceship',
-                scene: scene
+                name: 'hyper-spaceship',
+                scene: scene,
+                heartbeat: function (input) {
+                    var name = input && input.name;
+                    var scene = input && input.scene;
+                    var sprite;
+
+                    if (!name || !scene) {
+                        throw new Error(ebg.err.input.required);
+                    }
+
+                    sprite = scene.getObjectByName(name);
+
+                    if (!sprite) {
+                        return; // Sprite hasn't been found yet, it has probably not finished loading.
+                    }
+
+                    // Only run code below this point once the sprite has been found in the scene.
+                    
+                    if (sprite.position.y > 0) {
+                        // First vector: the spaceship slowly comes into view, losing altitude.
+                        sprite.position.z += 0.05;
+                        sprite.position.y += -0.05;
+                    } else {
+                        // Second vector: the spaceship speeds away from field of camera.
+                        sprite.position.z += 1.2;
+                    }
+
+                    if (sprite.position.z > 25) {
+                        sprite.position.set(-5, 20, -50); // back to start position.
+                    }
+                }
             },
             {
                 name: 'fast-spaceship',
