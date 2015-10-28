e605eb62275ebf12c0c070ff810e90b5a79a34fa
Nadjib Amar <hello@ebabel.eu>
2015-10-28 11:17:00 +0100

    migrating to webpack and bundle

---
+++ src_to_delete_soon/js/scripts/react.js
@@ -0,0 +1,6 @@
+(function() {
+    var react = require('react');
+
+    
+
+}());
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	entry: './app/components/Main.js',
+	output: {
+		filename: 'public/bundle.js'
+	},
+	module: {
+		loaders: [
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel'
+		]
+	}
+};
