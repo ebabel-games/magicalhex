fc71c273054f83ba41a754b58ba38adaac89f8b1
nadjibamar <hello@ebabel.eu>
2015-12-02 13:13:20 +0100

    small adjustment to actions timer

--- src/components/game/render/index.js
+++ src/components/game/render/index.js
@@ -20,11 +20,11 @@ const render = function render (input) {
 
     renderer.render(scene, camera);
 
-    // Mob actions run from here, 
-    // once per actionsTimer when it hits 0.5 second.
-    // Display all mob names: input.mob.children.map(function (_mob) { return _mob.userData.targetName });
+    // Mob actions run from here, once per actionsTimer.
+    // Display all mob names: 
+    // input.mob.children.map(function (_mob) { return _mob.userData.targetName });
     input.actionsTimer = input.actionsTimer + delta;
-    if (input.actionsTimer > 0.5) {
+    if (input.actionsTimer > 0.25) {
         mob.children.map(function (_mob) {
             executeMobActions(_mob);
         });
