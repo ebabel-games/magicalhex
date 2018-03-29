8a3a687b9cf422682fc16331ddd4ded26d3668ac
Nadjib Amar <hello@ebabel.eu>
2018-03-29 23:01:13 +0200

    debub mode improved

--- src/modules/debug.js
+++ src/modules/debug.js
@@ -1,7 +1,9 @@
 define([], () => {
-  // Set window.debug to switch from normal gameplay to super powered.
+  // Set window.debugMode to switch from normal gameplay to super powered.
+  console.log('Set window.debugMode to true to enable the debug mode.');
+
   const debug = (playerMovement, camera) => {
-    if (window.debug) {
+    if (window.debugMode) {
       playerMovement.forwardSpeed = 1;
       playerMovement.backwardSpeed = 1;
       document.getElementById('debugX').textContent = Math.round(camera.position.x * 10) / 10;
